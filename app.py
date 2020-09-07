913616c82f376bdb17d0f01924e57eae6189ae6c
Dorcas-Amoo <doorcars@gmail.com>
2020-09-07 16:18:06 +0000

    Edited Procfile and added SECRET_KEY to app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ if path.exists("envvar.py"):
 
 app = Flask(__name__)
 
-
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
