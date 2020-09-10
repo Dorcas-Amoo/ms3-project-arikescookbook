626bcb84467f9944494119a8c2c40ffa56e35b58
Dorcas-Amoo <doorcars@gmail.com>
2020-09-10 19:30:44 +0000

    Added insert_recipe function and rearranged functions

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 from os import path
 if path.exists("envvar.py"):
-  import envvar
+    import envvar
 
 
 app = Flask(__name__)
@@ -29,6 +29,30 @@ def recipes():
     return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 
+@app.route("/create_recipe")
+def create_recipe():
+    return render_template("create.html", categories=mongo.db.categories.find(), cuisine=mongo.db.cuisine.find())
+
+
+@app.route("/insert_recipe", methods=["POST"])
+def insert_recipe():
+    recipe = mongo.db.recipes
+    recipe.insert_one(
+        {
+            "category_name": request.form.get("category_name"),
+            "cuisine_type": request.form.get("category_name"),
+            "recipe_name": request.form.get("recipe_name"),
+            "description": request.form.get("description"),
+            "cooking_time": request.form.get("cooking_time"),
+            "ingredients": request.form.get("ingredients"),
+            "instructions": request.form.get("instructions"),
+            "servings": request.form.get("servings"),
+            "created_by": request.form.get("created_by"),
+            "date_created": request.form.get("date_created"),
+        })
+    return redirect(url_for('recipe_info', recipe_id=recipe._id))
+
+
 @app.route("/recipe_info/<recipe_id>")
 def recipe_info(recipe_id):
     that_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
@@ -41,11 +65,6 @@ def edit_recipe(recipe_id):
     all_categories = mongo.db.categories.find()
     all_cuisine = mongo.db.cuisine.find()
     return render_template("editrecipe.html", recipe=that_recipe, cuisine=all_cuisine, categories=all_categories)
-
-
-@app.route("/create_recipe")
-def create_recipe():
-    return render_template("create.html", categories=mongo.db.categories.find(), cuisine=mongo.db.cuisine.find())
 
 
 @app.route("/shop")
