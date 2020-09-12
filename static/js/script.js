c1070b6fd09e8dc2d22e243cefc9a041aaa3cff8
Dorcas-Amoo <doorcars@gmail.com>
2020-09-12 14:28:07 +0000

    Uploaded images, added carousel to base.html and script.js, styled in css and changed script.js path in base.html

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
     $('.collapsible').collapsible();
     $("select").material_select();
+    $('.carousel.carousel-slider').carousel({fullWidth: true});
+    setInterval(function() {
+        $(".carousel").carousel("next");
+    }, 2500);
   });
 
 $('.datepicker').pickadate({
