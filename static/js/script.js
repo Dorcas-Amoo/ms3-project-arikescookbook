3fcd8066fa8efc42ad74f119551dba3bc5ee1df0
Dorcas-Amoo <doorcars@gmail.com>
2020-09-14 04:19:03 +0000

    Added comments to script.js, edited css and added comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,17 @@
 $(document).ready(function(){
+    //To initialise the Accordion
     $('.collapsible').collapsible();
+    //To initialise dropdown selection
     $("select").material_select();
+    //To initialise side nav
     $(".button-collapse").sideNav();
+    //To initialise carousel image slider
     $('.carousel.carousel-slider').carousel({fullWidth: true});
     setInterval(function() {
         $(".carousel").carousel("next");
     }, 2500);
   });
-
+//To initialise datepicker
 $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year,
@@ -16,7 +20,7 @@ $('.datepicker').pickadate({
     close: 'Ok',
     closeOnSelect: false // Close upon selecting a date,
   });
-
+//To ensure the datepicker works
   document.getElementById("matfix").addEventListener("click", function(e) {
 	e.stopPropagation();
 });
